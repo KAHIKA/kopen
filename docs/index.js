ad4297f8224e6c5d31b67f109d6f41df26325f43
KAHIKA <kahika@net90-dhcp-17.sfc.keio.ac.jp>
2017-07-13 12:10:47 +0900

    発表版
    発表お疲れーーーーい！！！！

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,10 @@
-
 let point = 0;
-let question = "Laughter is timeless. Imagination has no age. And dreams are forever."
+let question =
+  "Laughter is timeless. Imagination has no age. And dreams are forever."
+let questionlist =
+  ["Laughter is timeless. Imagination has no age. And dreams are forever."]
 
-
-
-let questionlist = ["Laughter is timeless. Imagination has no age. And dreams are forever.", "asdf"]
-
-
-
-　function selectQestion(num) {
+function selectQestion(num) {
   question = questionlist[num];
   return question;
 }
@@ -16,10 +12,7 @@ let questionlist = ["Laughter is timeless. Imagination has no age. And dreams ar
 /*
  keyイベントのハンドラ
  */
-
 function keyup(event) {
-
-
   let output = document.querySelector("#output2");
   output = output.value
   let pointElement = document.querySelector("#point");
@@ -29,28 +22,28 @@ function keyup(event) {
 /*
 文字自動文字表示部分
 */
-
-
 function autoWriting(index) {
-  let toShow = question.slice(0, index);  //slice配列から一部を取り出して新しい配列を作る。0からindexまで
-  // 以下は適切に直してください
+  //slice配列から一部を取り出して新しい配列を作る。0からindexまで
+  let toShow = question.slice(0, index);
   console.log(toShow);
   let element = document.querySelector("#question2");
   element.textContent = toShow;
 }
 
-var TF = 0;
 
-function autoWritingLoop(maxCount, i, time) { //1秒ごとに文字を自動表示
+// var TF = 0; ちょっとわかんないからとりあえずなかったことにする！
+
+
+//1秒ごとに文字を自動表示
+function autoWritingLoop(maxCount, i, time) {
   if (i <= maxCount) {
     autoWriting(i);
-    setTimeout(function () { autoWritingLoop(maxCount, ++i, time/*, TF*/) }, time);  //1000ミリ秒
+    setTimeout(function () { autoWritingLoop(maxCount, ++i, time) }, time);  //1000ミリ秒
   }
   else if (i >= maxCount) {
     alert("finish!");
   }
 }
-
 
 function start(time) {
   let questionElement = document.querySelector("#question");
@@ -60,9 +53,9 @@ function start(time) {
   autoWritingLoop(maxCount, i, time)
 }
 
-
-//autoWritingLoop(文字数, 0)
-
+/*
+autoWritingLoop(文字数, 0)
+*/
 function go_page(next_page, delet_page) {
   console.log(next_page, delet_page);
   document.getElementById("page2").style.display = "block";
@@ -70,17 +63,17 @@ function go_page(next_page, delet_page) {
   console.log("hello");
 }
 
-
 function main() {
-  document.body.addEventListener("keyup", keyup); // body要素で起きるキーイベントのハンドラを設定
-  document.body.focus(); // body要素にキーボードフォーカスを移動
+  // body要素で起きるキーイベントのハンドラを設定
+  document.body.addEventListener("keyup", keyup);
+  // body要素にキーボードフォーカスを移動 
+  document.body.focus();
   let questionElement = document.querySelector("#question");
   questionElement.textContent = question;
 }
 
-
-window.addEventListener("load", main);　//これがあると全部をロード(読み込んだ)した後に動く
-
+//これがあると全部をロード(読み込んだ)した後に動く
+window.addEventListener("load", main);
 
 
 
